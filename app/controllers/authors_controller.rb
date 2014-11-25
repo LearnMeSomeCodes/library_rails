8793bb5bc14ec1e2747ff254fb5ddfbf3398f1f5
Adam Bray <adam.bray@yale.edu>
2014-11-24 21:36:04 -0500

    added in class work

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,6 +10,43 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  # GET /authors/new
+  def new
+    @author = Author.new()
+  end
+
+  # POST /authors
+  def create
+    # write code to create an author using the author info hash
+    @author = Author.new(author_params)
+    if @author.save
+      #success case
+      redirect_to author_path(@author)
+    else
+      render 'new'
+    end
+  end
+
+  # write the edit and update actions and view(s) as needed so we can edit existing authors
+  # the form to edit an author should be pre-populated
+  # GET /authors/:id/edit
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  # PUT /authors/:id
+  # modify the update action so that it behaves like our 'create' action
+  # that is, it should redisplay the form if you update an object that has errors
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render 'edit'
+    end
+  end
+
   # DELETE /authors/1
   def destroy
     @author = Author.find(params[:id])
@@ -17,5 +54,10 @@ class AuthorsController < ApplicationController
 
     redirect_to authors_path
   end
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :nationality, :birth_year)
+  end
+
 
 end
