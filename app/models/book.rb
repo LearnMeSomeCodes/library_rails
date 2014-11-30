edc57b864c30ca484a8d1b581b0854d57d875651
Brian Lauber <constructible.truth@gmail.com>
2014-11-30 18:09:03 -0500

    Added Book#author_name

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ActiveRecord::Base
   belongs_to :author
+
+  validates :title, :publication_date, :author, presence: true
+
+  # Returns the full name of the book's author,
+  # or nil if no author is defined yet.
+  def author_name
+    author.full_name if author
+  end
 end
