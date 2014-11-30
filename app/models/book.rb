5a1ea9629f265f7b7f5958d19e857df754c861d5
Brian Lauber <constructible.truth@gmail.com>
2014-11-30 18:16:27 -0500

    Book publication dates must be numbers

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
   belongs_to :author
 
   validates :title, :publication_date, :author, presence: true
-
+  validates :publication_date, numericality: true
   # Returns the full name of the book's author,
   # or nil if no author is defined yet.
   def author_name
